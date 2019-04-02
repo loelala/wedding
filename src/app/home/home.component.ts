5edb7a0531e32431d607c4665a35c7529870e965
Alexandra <alexandra.wenzel.mail@gmail.com>
2019-04-02 22:55:26 +0200

    added fading background color

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+declare const window: any;
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public opacity = 0;
+  public opacityReverse = 1;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const num = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log('NUM', num);
+    this.opacity = 1 - (1 - num / 3000);
+    this.opacityReverse = 1 - num / 3000;
+    console.log('OPACITY', this.opacity);
+    console.log('===================================');
+  }
 }
